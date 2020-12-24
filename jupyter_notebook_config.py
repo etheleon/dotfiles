3efb76b748468b986b01a355980ecbf552ba3fe4
etheleon <picy2k@gmail.com>
2020-12-25 01:37:40 +0800

    Update jupyter_notebook_config.py
    no longer required.

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -766,7 +766,6 @@
 
 c.NotebookApp.ip = '*'
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.open_browser = False
 
 # from s3contents import S3ContentsManager
 from pgcontents.hybridmanager import HybridContentsManager
